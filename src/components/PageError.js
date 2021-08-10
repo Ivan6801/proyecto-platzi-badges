3476d26085c816eac2d7f350694f9be9379bb3ee
Ivan Gonzalez <hal308366@gmail.com>
2021-08-10 12:53:02 -0700

    gh-pages a api

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './styles/PageError.css';
 
 function PageError (props) {
-    return <div className="PageError">❌{props.error.message}😱</div>;
+    return (
+            <div className="PageError">❌{props.error.message}😱
+            <br />
+                <h1>Lo siento no hay conexion a api 🙂</h1>
+            </div>
+
+        );
 }
 
 export default PageError;
