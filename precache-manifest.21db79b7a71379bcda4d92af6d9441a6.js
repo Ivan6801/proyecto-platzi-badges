8a80b53506adb5eeaf2bba5cb7662567acef890e
Ivan Gonzalez <hal308366@gmail.com>
2021-08-10 12:36:26 -0700

    Updates

---
+++ precache-manifest.21db79b7a71379bcda4d92af6d9441a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "997f5b1005d35998df0d",
+    "url": "/proyecto-platzi-badges/static/css/main.a07db4a2.chunk.css"
+  },
+  {
+    "revision": "997f5b1005d35998df0d",
+    "url": "/proyecto-platzi-badges/static/js/main.997f5b10.chunk.js"
+  },
+  {
+    "revision": "4810eea739a775836c2a",
+    "url": "/proyecto-platzi-badges/static/js/runtime~main.4810eea7.js"
+  },
+  {
+    "revision": "b8c50a2ab55a2082a601",
+    "url": "/proyecto-platzi-badges/static/css/2.a9df79d0.chunk.css"
+  },
+  {
+    "revision": "b8c50a2ab55a2082a601",
+    "url": "/proyecto-platzi-badges/static/js/2.b8c50a2a.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/proyecto-platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/proyecto-platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/proyecto-platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/proyecto-platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "bb5cd7e8e8db321148fb05cff02b6b76",
+    "url": "/proyecto-platzi-badges/static/media/404.bb5cd7e8.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/proyecto-platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "e740cc0fcfdc80f227fdebe4f0bd5334",
+    "url": "/proyecto-platzi-badges/index.html"
+  }
+];
